273f1a3caa76f6e9545ac4d50f4ffcebf5182335
Valeriy Malishevskyi <stalkermv@ukr.net>
2025-05-04 22:13:15 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppHooksUI",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppHooksUI",
+            targets: ["AppHooksUI"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppHooksUI"),
+        .testTarget(
+            name: "AppHooksUITests",
+            dependencies: ["AppHooksUI"]
+        ),
+    ]
+)
